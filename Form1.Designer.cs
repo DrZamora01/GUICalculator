c0e050e5dcd311fd31c1a69ae62f26f2f1583650
DrZamora01 <112430390+DrZamora01@users.noreply.github.com>
2022-11-21 14:48:14 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace GUICalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sevenBtn = new System.Windows.Forms.Button();
+            this.fourBtn = new System.Windows.Forms.Button();
+            this.oneBtn = new System.Windows.Forms.Button();
+            this.zeroBtn = new System.Windows.Forms.Button();
+            this.dotBtn = new System.Windows.Forms.Button();
+            this.twoBtn = new System.Windows.Forms.Button();
+            this.fiveBtn = new System.Windows.Forms.Button();
+            this.eightBtn = new System.Windows.Forms.Button();
+            this.enterBtn = new System.Windows.Forms.Button();
+            this.threeBtn = new System.Windows.Forms.Button();
+            this.sixBtn = new System.Windows.Forms.Button();
+            this.nineBtn = new System.Windows.Forms.Button();
+            this.percentBtn = new System.Windows.Forms.Button();
+            this.divideBtn = new System.Windows.Forms.Button();
+            this.minusBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.plusMinusBtn = new System.Windows.Forms.Button();
+            this.multiBtn = new System.Windows.Forms.Button();
+            this.plusBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(51, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(337, 42);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // sevenBtn
+            // 
+            this.sevenBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sevenBtn.Location = new System.Drawing.Point(40, 85);
+            this.sevenBtn.Name = "sevenBtn";
+            this.sevenBtn.Size = new System.Drawing.Size(53, 46);
+            this.sevenBtn.TabIndex = 1;
+            this.sevenBtn.Text = "7";
+            this.sevenBtn.UseVisualStyleBackColor = true;
+            this.sevenBtn.Click += new System.EventHandler(this.sevenBtn_Click);
+            // 
+            // fourBtn
+            // 
+            this.fourBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fourBtn.Location = new System.Drawing.Point(40, 137);
+            this.fourBtn.Name = "fourBtn";
+            this.fourBtn.Size = new System.Drawing.Size(53, 46);
+            this.fourBtn.TabIndex = 2;
+            this.fourBtn.Text = "4";
+            this.fourBtn.UseVisualStyleBackColor = true;
+            this.fourBtn.Click += new System.EventHandler(this.fourBtn_Click);
+            // 
+            // oneBtn
+            // 
+            this.oneBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.oneBtn.Location = new System.Drawing.Point(40, 189);
+            this.oneBtn.Name = "oneBtn";
+            this.oneBtn.Size = new System.Drawing.Size(53, 46);
+            this.oneBtn.TabIndex = 3;
+            this.oneBtn.Text = "1";
+            this.oneBtn.UseVisualStyleBackColor = true;
+            this.oneBtn.Click += new System.EventHandler(this.oneBtn_Click);
+            // 
+            // zeroBtn
+            // 
+            this.zeroBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zeroBtn.Location = new System.Drawing.Point(99, 241);
+            this.zeroBtn.Name = "zeroBtn";
+            this.zeroBtn.Size = new System.Drawing.Size(53, 46);
+            this.zeroBtn.TabIndex = 4;
+            this.zeroBtn.Text = "0";
+            this.zeroBtn.UseVisualStyleBackColor = true;
+            this.zeroBtn.Click += new System.EventHandler(this.zeroBtn_Click);
+            // 
+            // dotBtn
+            // 
+            this.dotBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dotBtn.Location = new System.Drawing.Point(371, 137);
+            this.dotBtn.Name = "dotBtn";
+            this.dotBtn.Size = new System.Drawing.Size(53, 46);
+            this.dotBtn.TabIndex = 8;
+            this.dotBtn.Text = ".";
+            this.dotBtn.UseVisualStyleBackColor = true;
+            this.dotBtn.Click += new System.EventHandler(this.dotBtn_Click);
+            // 
+            // twoBtn
+            // 
+            this.twoBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.twoBtn.Location = new System.Drawing.Point(99, 189);
+            this.twoBtn.Name = "twoBtn";
+            this.twoBtn.Size = new System.Drawing.Size(53, 46);
+            this.twoBtn.TabIndex = 7;
+            this.twoBtn.Text = "2";
+            this.twoBtn.UseVisualStyleBackColor = true;
+            this.twoBtn.Click += new System.EventHandler(this.twoBtn_Click);
+            // 
+            // fiveBtn
+            // 
+            this.fiveBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fiveBtn.Location = new System.Drawing.Point(99, 137);
+            this.fiveBtn.Name = "fiveBtn";
+            this.fiveBtn.Size = new System.Drawing.Size(53, 46);
+            this.fiveBtn.TabIndex = 6;
+            this.fiveBtn.Text = "5";
+            this.fiveBtn.UseVisualStyleBackColor = true;
+            this.fiveBtn.Click += new System.EventHandler(this.fiveBtn_Click);
+            // 
+            // eightBtn
+            // 
+            this.eightBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eightBtn.Location = new System.Drawing.Point(99, 85);
+            this.eightBtn.Name = "eightBtn";
+            this.eightBtn.Size = new System.Drawing.Size(53, 46);
+            this.eightBtn.TabIndex = 5;
+            this.eightBtn.Text = "8";
+            this.eightBtn.UseVisualStyleBackColor = true;
+            this.eightBtn.Click += new System.EventHandler(this.eightBtn_Click);
+            // 
+            // enterBtn
+            // 
+            this.enterBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterBtn.Location = new System.Drawing.Point(312, 241);
+            this.enterBtn.Name = "enterBtn";
+            this.enterBtn.Size = new System.Drawing.Size(112, 46);
+            this.enterBtn.TabIndex = 12;
+            this.enterBtn.Text = "=";
+            this.enterBtn.UseVisualStyleBackColor = true;
+            // 
+            // threeBtn
+            // 
+            this.threeBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.threeBtn.Location = new System.Drawing.Point(158, 189);
+            this.threeBtn.Name = "threeBtn";
+            this.threeBtn.Size = new System.Drawing.Size(53, 46);
+            this.threeBtn.TabIndex = 11;
+            this.threeBtn.Text = "3";
+            this.threeBtn.UseVisualStyleBackColor = true;
+            this.threeBtn.Click += new System.EventHandler(this.threeBtn_Click);
+            // 
+            // sixBtn
+            // 
+            this.sixBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sixBtn.Location = new System.Drawing.Point(158, 137);
+            this.sixBtn.Name = "sixBtn";
+            this.sixBtn.Size = new System.Drawing.Size(53, 46);
+            this.sixBtn.TabIndex = 10;
+            this.sixBtn.Text = "6";
+            this.sixBtn.UseVisualStyleBackColor = true;
+            this.sixBtn.Click += new System.EventHandler(this.sixBtn_Click);
+            // 
+            // nineBtn
+            // 
+            this.nineBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nineBtn.Location = new System.Drawing.Point(158, 85);
+            this.nineBtn.Name = "nineBtn";
+            this.nineBtn.Size = new System.Drawing.Size(53, 46);
+            this.nineBtn.TabIndex = 9;
+            this.nineBtn.Text = "9";
+            this.nineBtn.UseVisualStyleBackColor = true;
+            this.nineBtn.Click += new System.EventHandler(this.nineBtn_Click);
+            // 
+            // percentBtn
+            // 
+            this.percentBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.percentBtn.Location = new System.Drawing.Point(371, 189);
+            this.percentBtn.Name = "percentBtn";
+            this.percentBtn.Size = new System.Drawing.Size(53, 46);
+            this.percentBtn.TabIndex = 16;
+            this.percentBtn.Text = "%";
+            this.percentBtn.UseVisualStyleBackColor = true;
+            this.percentBtn.Click += new System.EventHandler(this.percentBtn_Click);
+            // 
+            // divideBtn
+            // 
+            this.divideBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divideBtn.Location = new System.Drawing.Point(253, 189);
+            this.divideBtn.Name = "divideBtn";
+            this.divideBtn.Size = new System.Drawing.Size(53, 46);
+            this.divideBtn.TabIndex = 15;
+            this.divideBtn.Text = "/";
+            this.divideBtn.UseVisualStyleBackColor = true;
+            this.divideBtn.Click += new System.EventHandler(this.divideBtn_Click);
+            // 
+            // minusBtn
+            // 
+            this.minusBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusBtn.Location = new System.Drawing.Point(253, 137);
+            this.minusBtn.Name = "minusBtn";
+            this.minusBtn.Size = new System.Drawing.Size(53, 46);
+            this.minusBtn.TabIndex = 14;
+            this.minusBtn.Text = "-";
+            this.minusBtn.UseVisualStyleBackColor = true;
+            this.minusBtn.Click += new System.EventHandler(this.minusBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(318, 85);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(106, 46);
+            this.clearBtn.TabIndex = 13;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            // 
+            // plusMinusBtn
+            // 
+            this.plusMinusBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusMinusBtn.Location = new System.Drawing.Point(253, 241);
+            this.plusMinusBtn.Name = "plusMinusBtn";
+            this.plusMinusBtn.Size = new System.Drawing.Size(53, 46);
+            this.plusMinusBtn.TabIndex = 20;
+            this.plusMinusBtn.Text = "-/+";
+            this.plusMinusBtn.UseVisualStyleBackColor = true;
+            this.plusMinusBtn.Click += new System.EventHandler(this.plusMinusBtn_Click);
+            // 
+            // multiBtn
+            // 
+            this.multiBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiBtn.Location = new System.Drawing.Point(312, 189);
+            this.multiBtn.Name = "multiBtn";
+            this.multiBtn.Size = new System.Drawing.Size(53, 46);
+            this.multiBtn.TabIndex = 19;
+            this.multiBtn.Text = "*";
+            this.multiBtn.UseVisualStyleBackColor = true;
+            this.multiBtn.Click += new System.EventHandler(this.multiBtn_Click);
+            // 
+            // plusBtn
+            // 
+            this.plusBtn.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusBtn.Location = new System.Drawing.Point(312, 137);
+            this.plusBtn.Name = "plusBtn";
+            this.plusBtn.Size = new System.Drawing.Size(53, 46);
+            this.plusBtn.TabIndex = 18;
+            this.plusBtn.Text = "+";
+            this.plusBtn.UseVisualStyleBackColor = true;
+            this.plusBtn.Click += new System.EventHandler(this.plusBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 403);
+            this.Controls.Add(this.plusMinusBtn);
+            this.Controls.Add(this.multiBtn);
+            this.Controls.Add(this.plusBtn);
+            this.Controls.Add(this.percentBtn);
+            this.Controls.Add(this.divideBtn);
+            this.Controls.Add(this.minusBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.enterBtn);
+            this.Controls.Add(this.threeBtn);
+            this.Controls.Add(this.sixBtn);
+            this.Controls.Add(this.nineBtn);
+            this.Controls.Add(this.dotBtn);
+            this.Controls.Add(this.twoBtn);
+            this.Controls.Add(this.fiveBtn);
+            this.Controls.Add(this.eightBtn);
+            this.Controls.Add(this.zeroBtn);
+            this.Controls.Add(this.oneBtn);
+            this.Controls.Add(this.fourBtn);
+            this.Controls.Add(this.sevenBtn);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button sevenBtn;
+        private System.Windows.Forms.Button fourBtn;
+        private System.Windows.Forms.Button oneBtn;
+        private System.Windows.Forms.Button zeroBtn;
+        private System.Windows.Forms.Button dotBtn;
+        private System.Windows.Forms.Button twoBtn;
+        private System.Windows.Forms.Button fiveBtn;
+        private System.Windows.Forms.Button eightBtn;
+        private System.Windows.Forms.Button enterBtn;
+        private System.Windows.Forms.Button threeBtn;
+        private System.Windows.Forms.Button sixBtn;
+        private System.Windows.Forms.Button nineBtn;
+        private System.Windows.Forms.Button percentBtn;
+        private System.Windows.Forms.Button divideBtn;
+        private System.Windows.Forms.Button minusBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button plusMinusBtn;
+        private System.Windows.Forms.Button multiBtn;
+        private System.Windows.Forms.Button plusBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUICalculator
+{
+    public partial class Form1 : Form
+    {
+        private decimal FirstNum = 0.0m;
+        private decimal SecondNum = 0.0m;
+        private decimal Results = 0.0m;
+        private string operators = "+";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void zeroBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "0";
+            }
+        }
+
+        private void oneBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text += "1";
+            }
+        }
+
+        private void twoBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text += "2";
+            }
+        }
+
+        private void threeBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text += "3";
+            }
+        }
+
+        private void fourBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text += "4";
+            }
+        }
+
+        private void fiveBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text += "5";
+            }
+        }
+
+        private void sixBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text += "6";
+            }
+        }
+        private void sevenBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text += "7";
+            }
+        }
+
+        private void eightBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text += "8";
+            }
+        }
+
+        private void nineBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text += "9";
+            }
+        }
+
+        private void dotBtn_Click(object sender, EventArgs e)
+        {
+            if(!textBox1.Text.Contains("."))
+            { 
+                textBox1.Text += ".";
+            }
+        }
+
+        private void plusMinusBtn_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Contains("-"))
+            {
+                textBox1.Text = textBox1.Text.Trim('-');
+            }
+            else
+            {
+                textBox1.Text = "-" + textBox1.Text;
+            }
+        }
+
+        private void minusBtn_Click(object sender, EventArgs e)
+        {
+            FirstNum = decimal.Parse(textBox1.Text);
+            textBox1.Clear();
+            operators = "-";
+        }
+        private void plusBtn_Click(object sender, EventArgs e)
+        {
+            FirstNum = decimal.Parse(textBox1.Text);
+            textBox1.Clear();
+            operators = "+";
+        }
+
+        private void divideBtn_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            operators = "/";
+        }
+
+        private void multiBtn_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            operators = "*";
+        }
+
+        private void percentBtn_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            operators = "%";
+        }
+    }
+}
